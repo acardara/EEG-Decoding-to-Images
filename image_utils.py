d4f3ff088e4d04fc7007a4c101622df002094040
AlexanderCardarasUCSC <75606039+AlexanderCardarasUCSC@users.noreply.github.com>
2021-04-23 16:24:01 -0700

    Added comments

--- image_utils.py
+++ image_utils.py
@@ -1,12 +1,16 @@
 import numpy as np
 
+# load refactored image dataset containing 40 classes of 900(64x64x3) images. Output shape is (36000, 64, 64, 3)
+# each image class is separated by 900 indices. class_0 = images[0:900], class_1 = images[900:1800], ...
 def load_images():
   images = np.load("/content/data/images/images.npy")
   image_labels = np.load("/content/data/images/image_labels.npy")
   return images, image_labels
   
+# flatten image data from (36000, 64, 64, 3) to (36000, 12288)
 def flatten_images(data):
   return data.reshape((data.shape[0], 12288))
 
+# reshape image data from (36000, 12288) to (36000, 64, 64, 3) 
 def reshape_images(data):
   return data.reshape((data.shape[0],64,64,3))
