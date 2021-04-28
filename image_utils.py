7d51a8193048cdcaed5aa9867a277424854d0008
AlexanderCardarasUCSC <75606039+AlexanderCardarasUCSC@users.noreply.github.com>
2021-04-28 13:55:57 -0700

    Update image_utils.py
    added optional path arg for loading images

--- image_utils.py
+++ image_utils.py
@@ -2,9 +2,9 @@ import numpy as np
 
 # load refactored image dataset containing 40 classes of 900(64x64x3) images. Output shape is (36000, 64, 64, 3)
 # each image class is separated by 900 indices. class_0 = images[0:900], class_1 = images[900:1800], ...
-def load_images():
-  images = np.load("/content/data/images/images.npy")
-  image_labels = np.load("/content/data/images/image_labels.npy")
+def load_images(path_root="/content/"):
+  images = np.load(path_root+"data/images/images.npy")
+  image_labels = np.load(path_root+"data/images/image_labels.npy")
   return images, image_labels
   
 # flatten image data from (36000, 64, 64, 3) to (36000, 12288)
